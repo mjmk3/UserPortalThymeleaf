b2b7da3b8624fb2d1154e108695e9742da3b3495
Mohamad J Makki <mohamad.j.makki@gmail.com>
2024-03-11 00:57:10 +0300

    Thymeleaf

---
+++ src/main/java/app/userman/UserManApplication.java
@@ -0,0 +1,13 @@
+package app.userman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserManApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/userman/UserManApplicationTests.java
@@ -0,0 +1,13 @@
+package app.userman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
